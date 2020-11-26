b453dd8f8943ebc3973ad0c20e7cbda200c2deb3
naotomineta <71373810+naotomineta@users.noreply.github.com>
2020-11-27 01:28:29 +0900

    #32

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
   has_many :messages
   has_many :memos
