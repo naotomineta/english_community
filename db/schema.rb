87a7100ac7ba6eb1b4d73d1effec2fdd5bab2007
naotomineta <71373810+naotomineta@users.noreply.github.com>
2020-12-08 23:27:39 +0900

    #61

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_023350) do
+ActiveRecord::Schema.define(version: 2020_12_03_130704) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,16 @@ ActiveRecord::Schema.define(version: 2020_11_23_023350) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "follow_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
+  end
+
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.integer "toeic_id"
@@ -65,6 +75,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_023350) do
     t.string "name"
     t.string "image"
     t.string "email", default: "", null: false
+    t.text "introduction"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -79,5 +90,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_023350) do
   add_foreign_key "memos", "users"
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "relationships", "users"
+  add_foreign_key "relationships", "users", column: "follow_id"
   add_foreign_key "rooms", "users"
 end
